180c47525de49bef2bbee479ddc6d372a5d1b564
Rudra Mittal <rudramittal24@gmail.com>
2025-02-15 18:14:15 +0530

    cors fixing

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,9 +12,13 @@ const port = process.env.PORT || 3000;
 // Middleware
 app.use(cors({
   origin: [process.env.FRONTEND_URL||"", 'http://localhost:5173'],
+  methods:['GET','POST'],
 }));
 app.use(express.json());
-
+app.get("/", (req, res) => {
+  console.log("request received");
+  res.send("Hello World!");
+});
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/quizzes', quizRoutes);
