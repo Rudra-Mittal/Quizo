3cd7e317f3fa77426fec1c578fa5d7ebfdcfba6e
Rudra Mittal <rudramittal24@gmail.com>
2025-02-15 18:37:58 +0530

    listening

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,8 +8,8 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 // Middleware
+
 app.use(cors({
   origin: [process.env.FRONTEND_URL||"", 'http://localhost:5173'],
   methods:['GET','POST'],
@@ -23,6 +23,6 @@ app.get("/", (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/quizzes', quizRoutes);
 
-app.listen(port, () => {
+app.listen(3000,'0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
