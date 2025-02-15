ecf124bbe479bd2423f7c02a632ee92bc90bf4d7
Rudra Mittal <rudramittal24@gmail.com>
2025-02-15 18:58:25 +0530

    ~updated:methods

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ const port = 3000;
 
 app.use(cors({
   origin: [process.env.FRONTEND_URL||"", 'http://localhost:5173'],
-  methods:['GET','POST'],
+  methods:['GET','POST','PUT','DELETE'],
 }));
 app.use(express.json());
 app.get("/", (req, res) => {
